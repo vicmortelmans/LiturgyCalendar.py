97b8d0f01cd7d3b3ee7b732fda4e51e82f20c9ae
OpenShift guest <vic.mortelmans@telenet.be>
2013-04-02 10:48:17 -0400

    intermediate

--- ruleset.py
+++ ruleset.py
@@ -50,32 +50,58 @@ class Calendar:
         self.ruleset = ruleset
         self.options = options
         self.liturgical_days = self.ruleset.get_liturgical_days_as_dictionary()
+        for coordinates in self.liturgical_days.keys():
+            self.liturgical_days[coordinates].set_calendar(self)
         self.days = {}
         for year in self.options.years:
             for date in Library.all_days_of_liturgical_year(year):
                 self.days[date] = Day(date,year)
+                self.days[date].set_calendar(self)
                 
-
     def populate(self):
-        pass
+        for year in self.options.years:
+            self.populate_year(year)
+            
+    def populate_year(self,year):
+        for coordinates in self.liturgical_days.keys():
+            self.liturgical_days[coordinates].evaluate(year)
+
+    def link(self,liturgical_day,day):
+        coordinates = liturgical_day.coordinates
+        subset = liturgical_day.subset
+        date = day.date
+        year = day.year
+        self.liturgical_days[coordinates].days[year] = self.days[date]
+        self.days[date].liturgical_days[subset] = self.liturgical_days[coordinates]
 
 class Liturgical_day:
     
     def __init__(self,coordinates,subset):
+        self.calendar = None
         self.coordinates = ''
         self.subset = ''
         self.attributes = {}
         self.daterules = None
-        self.days = {}
-        # TODO store data from XML record as attributes
+        self.days = {} # references to days by year
+
+    def set_calendar(self,calendar):
+        self.calendar = calendar
+
+    def evaluate(year):
+        # date = evaluate daterules for this year
+        # self.calendar.link(self,self.calendar.days[date])
         
 class Day:
     
     def __init__(self,date,year):
-        self.liturgical_days = {}
+        self.calendar = None
         self.date = date
         self.year = year
+        self.liturgical_days = {} # references to liturgical days by subset
     
+    def set_calendar(self,calendar):
+        self.calendar = calendar
+
 class Library:
     
     """ contains functions for resolving daterules and coordinaterules
@@ -99,7 +125,31 @@ class Library:
         end = Library.first_day_of_liturgical_year(year + 1)
         count = (end - start).days
         return [start + timedelta(days = x) for x in range(0,count)]
+
+    @staticmethod
+    def evaluate_daterules(daterules):
+        # TODO action = daterules.tagname()
+        # TODO return Library.rules[action](daterules)
+
+    @staticmethod
+    def date(daterules):
+        # day = daterules.get('day')
+        # month = daterules.get('month')
+        # year = daterules.get('year')
+        # return Date(day,month,year)
         
+    @staticmethod
+    def weekday_before(daterules):
+        # inputrules = daterules.find('*')
+        # inputdate = Library.evaluate_daterules(inputrules)
+        # day = daterules.get('day')
+        # calculate the output date and return
+
+    rules = {
+        "date" : Library.date,
+        "weekday-before" : Library.weekday_before
+    }
+
 if __name__ == '__main__':
     ruleset = Ruleset()
     options = Options()
