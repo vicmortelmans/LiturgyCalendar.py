6df5157fdb812fc1590f400aa09b62588dda8c67
Vic Mortelmans <vicmortelmans@gmail.com>
2013-04-05 06:49:49 +0200

    adding rulesets

--- ruleset.py
+++ ruleset.py
@@ -6,7 +6,7 @@ class Ruleset:
 
     def __init__(self): # TODO parametrize to accept filename
         """ load the xml data """
-        self.tree = ET.parse('custom-ruleset-of.parametrized.xml-file.xml')
+        self.tree = ET.parse('custom-ruleset-of.xml')
         self.list_of_coordinaterules = self.tree.findall('.//coordinaterules')
 
     def get_list_of_subsets(self):
@@ -28,15 +28,18 @@ class Ruleset:
                 'name' : liturgical_day.findtext('name'),
                 'season' : liturgical_day.findtext('season'),
                 'rankname' : liturgical_day.findtext('rank'),
-                'rankvalue' : liturgical_day.find('rank').get('nr'),
+                'rankvalue' : int(liturgical_day.find('rank').get('nr')),
                 'vigil' : liturgical_day.findtext('vigil'),
-                'precedence' : liturgical_day.findtext('precedence'),
+                'precedence' : int(liturgical_day.findtext('precedence')),
                 'color' : liturgical_day.findtext('color')
             }
             liturgical_day_obj.daterules = liturgical_day.find('daterules')
             # add to dictionary
             dictionary[coordinates] = liturgical_day_obj
         return dictionary
+        
+    def get_coordinates_by_name(self,name):
+        return self.tree.findtext('.//liturgicalday[name="' + name + '"]/coordinates')
         
 class Options:
     
@@ -65,10 +68,32 @@ class Calendar:
             
     def populate_year(self,year):
         """ populate all liturgical days for all subsets for a single year """
-        evaluate_daterules = Evaluate_daterules(year)
+        daterules_evaluator = Evaluate_daterules(self,year)
         for coordinates in self.liturgical_days.keys():
-            date = evaluate_daterules(self.liturgical_days[coordinates].daterules)
-            self.link(self.liturgical_days[coordinates], self.days[date])
+            self.evaluate_daterules(daterules_evaluator,coordinates)
+
+    def evaluate_daterules(self,daterules_evaluator,coordinates):
+        """ entry point for resolving the date for a liturgical day and
+            storing the result in the liturgical_days and days dictionaries.
+            the logic is not here, because the call is forwarded to a Daterules_evaluator,
+            but this method takes care that
+            - if the date has already been resolved, the daterules aren't evaluated again
+            - no circular calls are made """
+        liturgical_day = self.liturgical_days[coordinates]
+        if liturgical_day.state == "evaluating":
+            raise NameError("Circular call of evaluate_daterules")
+        elif liturgical_day.state == "evaluated":
+            return liturgical_day.days[daterules_evaluator.year].date
+        else:
+            liturgical_day.state = "evaluating"
+            date = daterules_evaluator.evaluate_daterules(liturgical_day.daterules)
+            liturgical_day.state = "evaluated"
+            self.link(liturgical_day, self.days[date])
+            return date
+
+    def evaluate_daterules_by_name(self,daterules_evaluator,name):
+        coordinates = self.ruleset.get_coordinates_by_name(name)
+        return self.evaluate_daterules(daterules_evaluator, coordinates)
 
     def link(self,liturgical_day,day):
         """ create a mutual link between a liturgical_day and a day """
@@ -83,10 +108,11 @@ class Liturgical_day:
     
     def __init__(self,coordinates,subset):
         self.calendar = None
-        self.coordinates = ''
-        self.subset = ''
+        self.coordinates = coordinates
+        self.subset = subset
         self.attributes = {}
         self.daterules = None
+        self.state = '' # set and gotten by Calendar.evaluate_daterules exclusively
         self.days = {} # references to days by year
 
     def set_calendar(self,calendar):
@@ -127,48 +153,208 @@ class Library:
         count = (end - start).days
         return [start + timedelta(days = x) for x in range(0,count)]
 
+    @staticmethod
+    def weekday_index(day):
+        index = {
+            "Monday" : 0,
+            "Tuesday" : 1,
+            "Wednesday" : 2,
+            "Thursday" : 3,
+            "Friday" : 4,
+            "Saturday" : 5,
+            "Sunday" : 6
+        }
+        return index[day]
+
 class Evaluate_daterules:
     """ evaluation toolset to evaluate daterules in the context of a specific year """
     
-    def __init__(self,year):
+    def __init__(self,calendar,year):
+        self.calendar = calendar
         self.year = year
-
+        self.easter_date = None
+        self.rules = {
+            "date" : self.date,
+            "daterules" : self.daterules,
+            "days-after" : self.days_after,
+            "days-before" : self.days_before,
+            "easterdate" : self.easterdate,
+            "equals" : self.equals,
+            "if" : self.if_then_else,
+            "not-after" : self.not_after,
+            "or" : self.or_logic,
+            "relative-to" : self.relative_to,
+            "relative-to-next-years" : self.relative_to_next_years,
+            "test-day" : self.test_day,
+            "weekday-after" : self.weekday_after,
+            "weekday-before" : self.weekday_before,
+            "weekday-before-or-self" : self.weekday_before_or_self,
+            "weeks-after" : self.weeks_after,
+            "weeks-before" : self.weeks_before
+        }
+        
     def evaluate_daterules(self,daterules):
-        """ reads the element name and runs the according method """
+        """ reads the element name and runs the according method 
+            this method should only be called from the method in Calendar with the same name """
         action = daterules.tag
-        return self.rules[action](daterules)
+        date = self.rules[action](daterules)
+        return date
 
     def date(self,daterules):
-        day = daterules.get('day')
-        month = daterules.get('month')
+        day = int(daterules.get('day'))
+        month = int(daterules.get('month'))
         previous_year = daterules.get('year-1')
         year = self.year if previous_year != 'true' else self.year - 1
         return date(year,month,day)
         
-    def weekday_before(self,daterules):
+    def daterules(self,daterules):
         inputrules = daterules.find('*')
         date = self.evaluate_daterules(inputrules)
+        return date
+        
+    def days_after(self,daterules):
+        inputrules = daterules.find('*')
+        date = self.evaluate_daterules(inputrules)
+        count = int(daterules.get('nr'))
+        return date + timedelta(count)
+        
+    def days_before(self,daterules):
+        inputrules = daterules.find('*')
+        date = self.evaluate_daterules(inputrules)
+        count = int(daterules.get('nr'))
+        return date - timedelta(count)
+        
+    def easterdate(self,daterules):
+        if self.easter_date is None:
+            tree = ET.parse('liturgy.calendar.roman-rite.easterdates.xml')
+            the_date = tree.find('.//easterdate[year="' + repr(self.year) + '"]')
+            year = int(the_date.findtext("year"))
+            month = int(the_date.findtext("month"))
+            day = int(the_date.findtext("day"))
+            self.easter_date = date(year,month,day)
+        return self.easter_date
+        
+    def equals(self,daterules):
+        rules = daterules.findall('*')
+        firstrules = rules[0]
+        first = self.evaluate_daterules(firstrules)
+        secondrules = rules[1]
+        second = self.evaluate_daterules(secondrules)
+        return first == second
+        
+    def if_then_else(self,daterules):
+        testrules = daterules.find('test/*')
+        test = self.evaluate_daterules(testrules)
+        if test:
+            inputrules = daterules.find('then/*')
+        else:
+            inputrules = daterules.find('else/*')
+        date = self.evaluate_daterules(inputrules)
+        return date
+        
+    def not_after(self,daterules):
+        rules = daterules.findall('*')
+        firstrules = rules[0]
+        first = self.evaluate_daterules(firstrules)
+        secondrules = rules[1]
+        second = self.evaluate_daterules(secondrules)
+        return first <= second
+        
+    def or_logic(self,daterules):
+        rules = daterules.findall('*')
+        firstrules = rules[0]
+        first = self.evaluate_daterules(firstrules)
+        secondrules = rules[1]
+        second = self.evaluate_daterules(secondrules)
+        return first or second
+        
+    def relative_to(self,daterules):
+        name = daterules.get('name')
+        return self.calendar.evaluate_daterules_by_name(self,name)
+
+    def relative_to_next_years(self,daterules):
+        """ this daterule can only occur with "First Sunday of Advent" as name, 
+            so we're not bothering about reading the attribute """
+        return Library.first_day_of_liturgical_year(self.year + 1)
+        
+    def test_day(self,daterules):
+        inputrules = daterules.find('*')
+        date = self.evaluate_daterules(inputrules)
+        day = daterules.get('day')
+        actual_weekday = date.weekday()
+        test_weekday = Library.weekday_index(day)
+        return actual_weekday == test_weekday
+        
+    def weekday_after(self,daterules):
+        inputrules = daterules.find('*')
+        inputdate = self.evaluate_daterules(inputrules)
+        day = daterules.get('day')
+        # get the weekday index [0-6] of the date (r) and of the day (n)
+        # e.g. r is a Wednesday (3) and n is a Monday (1), so 5 days must be counted forwards
+        # e.g. r is a Wednesday (3) and n is a Saturday (6), so 3 days must be counted forwards
+        # e.g. r and n are both Mondays (1), so 7 days mus be counted forwards 
+        # in a simple formula, this is: (n-r-1) % 7 + 1
+        r = inputdate.weekday()
+        n = Library.weekday_index(day)
+        count = (n - r - 1) % 7 + 1
+        return inputdate + timedelta(count)
+
+    def weekday_before(self,daterules):
+        inputrules = daterules.find('*')
+        inputdate = self.evaluate_daterules(inputrules)
         day = daterules.get('day')
         # get the weekday index [0-6] of the date (r) and of the day (n)
         # e.g. r is a Wednesday (3) and n is a Monday (1), so 2 days must be counted backwards
         # e.g. r is a Wednesday (3) and n is a Saturday (6), so 4 days must be counted backwards
         # e.g. r and n are both Mondays (1), so 7 days mus be counted backwards 
         # in a simple formula, this is: (r-n-1) % 7 + 1
-        r = date.weekday()
+        r = inputdate.weekday()
+        n = Library.weekday_index(day)
         count = (r - n - 1) % 7 + 1
-        # inputrules = daterules.find('*')
-        # inputdate = Library.evaluate_daterules(inputrules)
-        # day = daterules.get('day')
-        # calculate the output date and return
+        return inputdate - timedelta(count)
 
-    @staticmethod
-    def weekday():
-        pass
+    def weekday_before_or_self(self,daterules):
+        inputrules = daterules.find('*')
+        inputdate = self.evaluate_daterules(inputrules)
+        day = daterules.get('day')
+        # get the weekday index [0-6] of the date (r) and of the day (n)
+        # e.g. r is a Wednesday (3) and n is a Monday (1), so 2 days must be counted backwards
+        # e.g. r is a Wednesday (3) and n is a Saturday (6), so 4 days must be counted backwards
+        # e.g. r and n are both Mondays (1), so 0 days mus be counted backwards 
+        # in a simple formula, this is: (r-n) % 7 
+        r = inputdate.weekday()
+        n = Library.weekday_index(day)
+        count = (r - n) % 7
+        return inputdate - timedelta(count)
 
-    rules = {
-        "date" : Library.date,
-        "weekday-before" : Library.weekday_before
-    }
+    def weeks_after(self,daterules):
+        inputrules = daterules.find('*')
+        inputdate = self.evaluate_daterules(inputrules)
+        count = int(daterules.get('nr'))
+        return inputdate + timedelta(7 * count)
+        
+    def weeks_before(self,daterules):
+        inputrules = daterules.find('*')
+        inputdate = self.evaluate_daterules(inputrules)
+        count = int(daterules.get('nr'))
+        return inputdate - timedelta(7 * count)
+        
+
+class Easter_dates:
+
+    def __init__(self): 
+        """ load the xml data """
+        self.easterdates = {}
+        tree = ET.parse('liturgy.calendar.roman-rite.easterdates.xml')
+        easterdates = tree.findall('.//easterdate')
+        for the_date in easterdates:
+            year = the_date.find("year")
+            month = the_date.find("month")
+            day = the_date.find("day")
+            self.easterdates[year] = date(year,month,day)
+
+    def get(self,year):
+        return self.easterdates[year]
 
 if __name__ == '__main__':
     ruleset = Ruleset()
---
+++ rulesets/liturgy.calendar.build-ruleset.xslt
@@ -0,0 +1,110 @@
+<?xml version="1.0"?>
+<xsl:stylesheet version="2.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+
+  <xsl:template match="/">
+    <xsl:apply-templates mode="build"/>
+  </xsl:template>
+
+  <xsl:template match="*" mode="build">
+    <xsl:copy-of select="."/>
+  </xsl:template>
+  
+  <xsl:template match="liturgicaldays" mode="build">
+    <xsl:copy>
+      <xsl:apply-templates mode="build"/>
+    </xsl:copy>
+  </xsl:template>
+  
+  <xsl:template match="includeliturgicaldays" mode="build">
+    <xsl:apply-templates select="doc(.)/liturgicaldays/*" mode="build"/>
+  </xsl:template>
+  
+  <xsl:template match="liturgicalday" mode="build">
+    <xsl:message>Processing set <xsl:value-of select="template/set"/></xsl:message>
+    <xsl:if test="parameters">
+      <xsl:variable name="liturgicalday" select="."/>
+      <xsl:variable name="parameters">
+        <xsl:apply-templates select="parameters" mode="build"/>
+      </xsl:variable>
+      <xsl:for-each select="$parameters/parametergroup">
+        <liturgicalday>
+          <xsl:apply-templates select="$liturgicalday/template/*" mode="fill-in">
+            <xsl:with-param name="parametergroup" select="."/>
+          </xsl:apply-templates>
+        </liturgicalday>
+      </xsl:for-each>
+    </xsl:if>
+    <xsl:if test="not(parameters)">
+      <liturgicalday>
+        <xsl:copy-of select="template/*"/>
+      </liturgicalday>
+    </xsl:if>
+  </xsl:template>
+
+  <!-- FLATTENING MULTIPLE LEVELS OF PARAMETRIZATION -->
+  <xsl:template match="parameters" mode="build">
+    <xsl:param name="accuparametergroup"/>
+    <xsl:for-each select="parametergroup">
+      <xsl:if test="../parameters">
+        <xsl:apply-templates select="../parameters" mode="build">
+          <xsl:with-param name="accuparametergroup">
+            <xsl:copy-of select="$accuparametergroup"/>
+            <xsl:copy-of select="*"/>
+          </xsl:with-param>
+        </xsl:apply-templates>
+      </xsl:if>
+      <xsl:if test="not(../parameters)">
+        <parametergroup>
+          <xsl:copy-of select="$accuparametergroup"/>
+          <xsl:copy-of select="*"/>
+        </parametergroup>
+      </xsl:if>
+    </xsl:for-each>
+  </xsl:template>
+
+  <!-- FILLING IN THE PARAMETERS INTO THE TEMPLATE -->
+  <xsl:template match="node()" mode="fill-in">
+    <xsl:param name="parametergroup"/>
+    <xsl:copy>
+      <xsl:apply-templates select="attribute::node()|child::node()" mode="fill-in">
+        <xsl:with-param name="parametergroup" select="$parametergroup"/>
+      </xsl:apply-templates>
+    </xsl:copy>
+  </xsl:template>
+
+  <xsl:template name="replace2">
+    <xsl:param name="string"/>
+    <xsl:param name="parametergroup"/>
+    <xsl:choose>
+      <xsl:when test="not(matches($string,'\$'))">
+        <xsl:value-of select="$string"/>
+      </xsl:when>
+      <xsl:otherwise>
+        <xsl:call-template name="replace2">
+          <xsl:with-param name="string" select="replace($string,concat('\$',local-name($parametergroup/*[1])),$parametergroup/*[1])"/>
+          <xsl:with-param name="parametergroup">
+            <xsl:copy-of select="$parametergroup/*[position() &gt; 1]"/>
+          </xsl:with-param>
+        </xsl:call-template>
+      </xsl:otherwise>
+    </xsl:choose>
+  </xsl:template>
+
+  <xsl:template match="text()" mode="fill-in" priority="2">
+    <xsl:param name="parametergroup"/>
+    <xsl:call-template name="replace2">
+      <xsl:with-param name="string" select="."/>
+      <xsl:with-param name="parametergroup" select="$parametergroup"/>
+    </xsl:call-template>
+  </xsl:template>
+
+  <xsl:template match="attribute::node()" mode="fill-in" priority="2">
+    <xsl:param name="parametergroup"/>
+    <xsl:attribute name="{local-name(.)}">
+      <xsl:call-template name="replace2">
+        <xsl:with-param name="string" select="."/>
+        <xsl:with-param name="parametergroup" select="$parametergroup"/>
+      </xsl:call-template>
+    </xsl:attribute>
+  </xsl:template>
+</xsl:stylesheet>
